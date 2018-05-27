2711c1357b08a33293586041203d3b939dbd82fd
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2018-05-27 19:11:08 +0100

    Fix namespace for excel functions

--- R/Excel.R
+++ R/Excel.R
@@ -3,7 +3,7 @@
   log <- capture.output({
     res <- tryCatch(
       {
-        excel_sheets(file)
+        readxl::excel_sheets(file)
       },
       error=function(e) {
         #Probably just wrong file extension, try xlsx
@@ -18,7 +18,7 @@
 #Read (if it fails then force xlsx) (supress prints)
 .xldata <- function(file, sheet) {
   log <- capture.output({
-    res <- tryCatch(readxl::read_excel(file,sheet), error = function(e) readxl:::read_xlsx(file,sheet))
+    res <- tryCatch(readxl::read_excel(file,sheet), error = function(e) readxl::read_xlsx(file,sheet))
   })
   return(res)
 }
