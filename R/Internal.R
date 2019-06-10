01bfc820b855060ba1fbf38e5ae1bca6390b53d3
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-10 14:42:05 +0100

    Improve internal import function

--- R/Internal.R
+++ R/Internal.R
@@ -1,18 +1,30 @@
 #' Read echem data that has previously been exported to a csv file
 #'
-#' This function is used to import echem data from a csv file that was generated using \code{\link{export_echem}}.
-#' @param file Path to data file
+#' This function is used to import echem data from a csv file that was generated using \code{\link{export}}.
+#' @param path Path to data file
 #' @return A data frame containing the echem data
 #' @examples
 #' load.internal("/path/to/file.csv")
 #' @keywords internal
-load.internal <- function(file) {
-  data <- readLines(file, n=20) #Assume no more than 19 attributes
-  headerRow <- grep("Voltage.V.",data)
-  atts=.get_internal_attributes(data[c(1:(headerRow-1))])
-  data=read.table(file, sep=",", skip = headerRow-1, header=TRUE)
-  for(n in names(atts)) {
-    attr(data,n)<-atts[n][[1]]
+load.internal <- function(path) {
+  #Open the file for reading
+  conn <- file(path, open="rt")
+  #Read until we find a line starting with #S
+  header <- c()
+  x <- TRUE
+  while (x) {
+    line <- readLines(conn, n=1)
+    header <- append(header, line)
+    x <- !grepl("^==== Data ====", line)
+  }
+
+  atts <- .get_internal_attributes(header[-c(1, length(header))])
+  data <- read.table.echem(conn, sep=",", header=TRUE)
+
+  close(conn)
+
+  for (n in names(atts)) {
+    attr(data, n) <- atts[n][[1]]
   }
   return(data)
 }
@@ -26,14 +38,17 @@ load.internal <- function(file) {
 #' @return A list of attributes that were found
 #' @keywords internal
 .get_internal_attributes <- function(header_text) {
-  atts=list()
-  header_text=strsplit(header_text,',')
-  for(line in header_text) {
-    attName=line[[1]]
-    attValue=line[[2]]
-    if(attName=='date') attValue=as.Date(attValue)
-    if(attName=='mass') attValue=as.numeric(attValue)
-    atts[[attName]]=attValue
+  atts <- list()
+  header_text <- strsplit(header_text, ",")
+  for (line in header_text) {
+    if (!length(line) == 2) {
+      next
+    }
+    attName <- line[[1]]
+    attValue <- line[[2]]
+    if (attName == "date") attValue <- as.Date(attValue)
+    if (attName == "mass") attValue <- as.numeric(attValue)
+    atts[[attName]] <- attValue
   }
   atts
 }
