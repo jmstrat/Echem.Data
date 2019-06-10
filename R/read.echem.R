c59d92b8c098c32eab6c266b00195c61f300f47d
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-10 14:54:33 +0100

    lint

--- R/read.echem.R
+++ R/read.echem.R
@@ -10,27 +10,29 @@
 #' read.echem("/path/to/file.xlsx")
 #' read.echem("/path/to/file.txt")
 #' read.echem("/path/to/file.idf")
-read.echem <-function(path,...) {
-  as.echem.data.object(jms.classes::load.jms(path,.read.echem,...))
+read.echem <- function(path, ...) {
+  jms.classes::load.jms(path, .read.echem, ...)
 }
 
-.read.echem <-function(echem_file,cycler_type=NA,...)
-{
-  if(is.null(echem_file)|is.na(echem_file)|echem_file=='')
+.read.echem <- function(echem_file, cycler_type=NA, ...) {
+  if (is.null(echem_file) || is.na(echem_file) || echem_file == "") {
+    jms.classes::log.warn("File not specified")
     return(NA)
-
-  jms.classes::log.info('Reading echem data from "%s"',echem_file)
-
-  if(!file.exists(echem_file)){
-    #file doesn't exist
-    stop("Unable to find file to load -- is the path typed correctly?",call. = FALSE)
   }
-  jms.classes::log.debug('"%s" exists.',echem_file)
-  if(is.na(cycler_type)) {
-    cycler_type=guess_cycler(echem_file)
+
+  jms.classes::log.info('Reading echem data from "%s"', echem_file)
+
+  if (!file.exists(echem_file)) {
+    # file doesn't exist
+    stop("Unable to find file to load -- is the path typed correctly?", call.=FALSE)
   }
-  cycler_types=names(cycler_types())
-  cycler_type=cycler_normalise(cycler_type)
+  jms.classes::log.debug('"%s" exists.', echem_file)
+  if (is.na(cycler_type)) {
+    cycler_type <- guess_cycler(echem_file)
+  }
+  cycler_types <- names(cycler_types())
+  cycler_type <- cycler_normalise(cycler_type)
+
   if (is.na(cycler_type)) stop(paste0("Unknown cycler type! Known cyclers are: ",
                                       paste0(cycler_types, collapse=", ")), call.=FALSE)
 
@@ -40,15 +42,15 @@ read.echem <-function(path,...) {
 
   if (all(is.null(echem))) return(NA)
 
+  jms.classes::log.info('Successfully loaded data for "%s"', echem_file)
 
+  echem <- as.echem.data.object(echem)
 
-  jms.classes::log.info('Successfully loaded data for "%s"',echem_file)
   jms.classes::xcol(echem) <- "Test_Time.s."
   jms.classes::xscale(echem) <- 1/3600
   jms.classes::xlab(echem) <- expression("Time / h")
   jms.classes::ycol(echem) <- "Voltage.V."
 
-  echem=as.echem.data.object(echem)
   # Check that columns were found
   xc <- jms.classes::xcol(echem)
   yc <- jms.classes::ycol(echem)
@@ -74,9 +76,9 @@ read.echem <-function(path,...) {
 
   jms.classes::log.debug("Set xcol = %s; ycol = %s; y2col = %s", xc, yc, yc2)
 
-  if(cycler_type=='internal') return(echem) #Attributes already added
-  #Add attributes:
-  attr(echem,'Cycler')<-cycler_type
+  if (cycler_type == "internal") return(echem) # Attributes already added
+  # Add attributes:
+  attr(echem, "Cycler") <- cycler_type
 
   echem
 }
