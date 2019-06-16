2f847a603884a22e3ae1703744809b6e8833a367
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-16 02:24:54 +0100

    lint

--- R/read.echem.R
+++ R/read.echem.R
@@ -33,22 +33,28 @@ read.echem <- function(path, ...) {
   cycler_types <- names(cycler_types())
   cycler_type <- cycler_normalise(cycler_type)
 
-  if (is.na(cycler_type)) stop(paste0("Unknown cycler type! Known cyclers are: ",
-                                      paste0(cycler_types, collapse=", ")), call.=FALSE)
+  if (is.na(cycler_type)) {
+    stop(paste0(
+      "Unknown cycler type! Known cyclers are: ",
+      paste0(cycler_types, collapse=", ")
+    ), call.=FALSE)
+  }
 
   jms.classes::log.info('Treating "%s" as a %s file', echem_file, cycler_type)
 
   echem <- loader_for_cycler(cycler_type, echem_file)
   echem <- load.addMissingColumns(echem)
 
-  if (all(is.null(echem))) return(NA)
+  if (all(is.null(echem))) {
+    return(NA)
+  }
 
   jms.classes::log.info('Successfully loaded data for "%s"', echem_file)
 
   echem <- as.echem.data.object(echem)
 
   jms.classes::xcol(echem) <- "Test_Time.s."
-  jms.classes::xscale(echem) <- 1/3600
+  jms.classes::xscale(echem) <- 1 / 3600
   jms.classes::xlab(echem) <- expression("Time / h")
   jms.classes::ycol(echem) <- "Voltage.V."
 
@@ -59,25 +65,31 @@ read.echem <- function(path, ...) {
   if (length(xc) != 1 || is.na(xc)) {
     jms.classes::log.warn(
       "xcol is not correct: xcol = [%s]",
-      paste0(xc, collapse=","))
+      paste0(xc, collapse=",")
+    )
     warning("Unable to find time column")
   }
   if (length(yc) != 1 || is.na(yc)) {
     jms.classes::log.warn(
       "ycol is not correct: ycol = [%s]",
-      paste0(yc, collapse=","))
+      paste0(yc, collapse=",")
+    )
     warning("Unable to find voltage column")
   }
   if (length(yc2) != 1 || !is.na(yc2)) {
     jms.classes::log.warn(
       "y2col is not correct: y2col = [%s]",
-      paste0(yc2, collapse=","))
+      paste0(yc2, collapse=",")
+    )
     warning("A second y axis has been specified")
   }
 
   jms.classes::log.debug("Set xcol = %s; ycol = %s; y2col = %s", xc, yc, yc2)
 
-  if (cycler_type == "internal") return(echem) # Attributes already added
+  if (cycler_type == "internal") {
+    # Attributes already added
+    return(echem)
+  }
   # Add attributes:
   attr(echem, "Cycler") <- cycler_type
 
