d83f06b0cf675a731668e14531ba3d1db81711da
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-16 02:19:06 +0100

    lint

--- R/Excel.R
+++ R/Excel.R
@@ -1,19 +1,20 @@
-#Get sheet names (if it fails then force xlsx) (supress prints)
+# Get sheet names (if it fails then force xlsx) (supress prints)
 .xlsheets <- function(file) {
   capture.output({
     res <- tryCatch({
-        readxl::excel_sheets(file)
-      }, error=function(e) {
-        # Probably just wrong file extension, try xlsx
-        tryCatch(readxl:::xlsx_sheets(file), error=function(e) {
-          warning("Unable to read file, please check the extension is correct (e.g. xlsx not xls)")
-          NULL
-        })})
+      readxl::excel_sheets(file)
+    }, error=function(e) {
+      # Probably just wrong file extension, try xlsx
+      tryCatch(readxl:::xlsx_sheets(file), error=function(e) {
+        warning("Unable to read file, please check the extension is correct (e.g. xlsx not xls)")
+        NULL
+      })
+    })
   })
   return(res)
 }
 
-#Read (if it fails then force xlsx) (supress prints)
+# Read (if it fails then force xlsx) (supress prints)
 .xldata <- function(file, sheet, ...) {
   capture.output({
     res <- tryCatch(readxl::read_excel(file, sheet, ...), error=function(e) readxl::read_xlsx(file, sheet, ...))
