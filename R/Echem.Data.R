317b60874ef69c2f8642d1f9434bb9b2ef7a61d9
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-16 11:52:21 +0100

    Better handling of automatically generated function exports

--- R/Echem.Data.R
+++ R/Echem.Data.R
@@ -9,8 +9,32 @@ NULL
   jms.classes::create_data_type("echem", expression("Time / s"), "Voltage / V", envir=asNamespace("Echem.Data"))
 }
 
-#' @export
-echem.data.object <- function(...) echem.data.object.super(...)
+# NOTE These functions are created in the .onLoad function and added to the namespace,
+# we need to export and document them here, if overrides to the default behaviour are
+# required, then a new function should be created under the same name, the default
+# function can be accessed as <function>.super(...)
 
+#' @title Echem data objects
+#' @param x The object
+#' @rdname data_objects_null_exports
+
+#' @name echem.data.object
+#' @usage echem.data.object(...)
 #' @export
-is.echem.data.object <- function(...) is.echem.data.object.super(...)
+#' @rdname data_objects_null_exports
+#' @keywords internal
+NULL
+
+#' @name is.echem.data.object
+#' @usage is.echem.data.object(x)
+#' @export
+#' @rdname data_objects_null_exports
+#' @keywords internal
+NULL
+
+#' @name as.echem.data.object
+#' @usage as.echem.data.object(...)
+#' @export
+#' @rdname data_objects_null_exports
+#' @keywords internal
+NULL
