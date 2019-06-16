6db3a2de7834c637e5035590d9cab208a91e263f
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-16 02:25:31 +0100

    lint

--- R/zzz_rexports.R
+++ R/zzz_rexports.R
@@ -1,62 +1,62 @@
-##' Reexported functions from jms.classes
-##' @name reexports
-##' @rdname jms.reexports
-##' @keywords internal
+#' Reexported functions from jms.classes
+#' @name reexports
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
 
-##' @importFrom jms.classes iplot
-##' @name iplot
-##' @export
-##' @rdname jms.reexports
-##' @keywords internal
+#' @importFrom jms.classes iplot
+#' @name iplot
+#' @export
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
 
-##' @importFrom jms.classes export
-##' @name export
-##' @export
-##' @rdname jms.reexports
-##' @keywords internal
+#' @importFrom jms.classes export
+#' @name export
+#' @export
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
 
-##' @importFrom jms.classes xlab
-##' @name xlab
-##' @export
-##' @rdname jms.reexports
-##' @keywords internal
+#' @importFrom jms.classes xlab
+#' @name xlab
+#' @export
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
 
-##' @importFrom jms.classes ylab
-##' @name ylab
-##' @export
-##' @rdname jms.reexports
-##' @keywords internal
+#' @importFrom jms.classes ylab
+#' @name ylab
+#' @export
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
 
-##' @importFrom jms.classes y2lab
-##' @name y2lab
-##' @export
-##' @rdname jms.reexports
-##' @keywords internal
+#' @importFrom jms.classes y2lab
+#' @name y2lab
+#' @export
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
 
 
-##' @importFrom jms.classes xcol
-##' @name xcol
-##' @export
-##' @rdname jms.reexports
-##' @keywords internal
+#' @importFrom jms.classes xcol
+#' @name xcol
+#' @export
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
 
-##' @importFrom jms.classes ycol
-##' @name ycol
-##' @export
-##' @rdname jms.reexports
-##' @keywords internal
+#' @importFrom jms.classes ycol
+#' @name ycol
+#' @export
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
 
-##' @importFrom jms.classes y2col
-##' @name y2col
-##' @export
-##' @rdname jms.reexports
-##' @keywords internal
+#' @importFrom jms.classes y2col
+#' @name y2col
+#' @export
+#' @rdname jms.reexports
+#' @keywords internal
 NULL
